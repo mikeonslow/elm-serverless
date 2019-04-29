d3b7c9309251c50d22bb700ec18d5ac2e196dd50
mikeonslow <mikeonslow@gmail.com>
2019-04-29 19:08:00 -0400

    Formatted with Prettier

--- src/lambda/process.js
+++ src/lambda/process.js
@@ -1,23 +1,22 @@
-require('dotenv').config();
-var request = require('request');
-var querystring = require('querystring');
+require("dotenv").config();
+var request = require("request");
+var querystring = require("querystring");
 
-exports.handler = (event, context, callback) => {
-
+exports.handler = async (event, context) => {
   const apiHost = process.env.API_HOST;
   const { body } = event;
   const filters = JSON.parse(body);
   const url = getUrl(apiHost, filters);
 
-  request.get(url, function (err, httpResponse, responseBody) {
+  request.get(url, function(err, httpResponse, responseBody) {
     console.log(err, httpResponse, responseBody);
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(responseBody),
+      body: JSON.stringify(responseBody)
     });
   });
 };
 
 function getUrl(apiHost, filters) {
-  return apiHost + '?' + querystring.stringify(filters);
+  return apiHost + "?" + querystring.stringify(filters);
 }
