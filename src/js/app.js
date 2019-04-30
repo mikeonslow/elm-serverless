61c05ae0cf48421268042a975a7e20bdc9d9db01
mikeonslow <mikeonslow@gmail.com>
2019-04-29 20:02:20 -0400

    WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,22 @@
-
-import '../../node_modules/bootstrap-scss/bootstrap.scss';
+import "../../node_modules/bootstrap-scss/bootstrap.scss";
 import { Elm } from "../elm/Main.elm";
 
-(function () {
-    var startup = function () {
-        // Start the Elm App.
+(function() {
+  var startup = function() {
+    // Start the Elm App.
 
-        var app = Elm.Main.init({
-            node: document.getElementById('main')
-        });
+    var app = Elm.Main.init({
+      node: document.getElementById("main")
+    });
 
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', function (data) {
-            console.log(data);
-        });
-        xhr.open('POST', '.netlify/functions/process');
-        xhr.setRequestHeader('content-type', 'application/json');
-        xhr.send(JSON.stringify({ 'by_state': 'michigan', 'by_city': 'ferndale' }));
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function(data) {
+      console.log(data.srcElement.response);
+    });
+    xhr.open("POST", ".netlify/functions/process");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(JSON.stringify({ by_state: "michigan", by_city: "ferndale" }));
+  };
 
-    }
-
-    window.addEventListener('load', startup, false);
-}());
+  window.addEventListener("load", startup, false);
+})();
