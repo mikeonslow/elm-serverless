004911becaafbc26aa90c7a8714fe5133670efdc
mikeonslow <mikeonslow@gmail.com>
2019-05-01 19:49:56 -0400

    Removed XHR call from JS

--- src/js/app.js
+++ src/js/app.js
@@ -8,14 +8,6 @@ import { Elm } from "../elm/Main.elm";
     var app = Elm.Main.init({
       node: document.getElementById("main")
     });
-
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function(data) {
-      console.log(data.srcElement.response);
-    });
-    xhr.open("POST", ".netlify/functions/process");
-    xhr.setRequestHeader("content-type", "application/json");
-    xhr.send(JSON.stringify({ by_state: "michigan", by_city: "ferndale" }));
   };
 
   window.addEventListener("load", startup, false);
