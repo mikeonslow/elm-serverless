cff88bc9fd13d9d8ec17beaa33db7bb257e0e21b
mikeonslow <mikeonslow@gmail.com>
2019-05-01 22:22:36 -0400

    Removed elm debug

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         use: {
           loader: "elm-webpack-loader",
           options: {
-            debug: true
+            debug: false
           }
         }
       },
